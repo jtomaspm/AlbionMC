2c7ad08a8907d0b6bb8c47e21aa2df1283e7ee1e
João Machado <jtomaspm@ua.pt>
2024-05-12 22:18:30 +0100

    remove navbar when user not logged in

--- apps/AlbionMC_frontend/src/components/navbar.tsx
+++ apps/AlbionMC_frontend/src/components/navbar.tsx
@@ -1,7 +1,8 @@
 import { Component } from "solid-js";
 import { UserWidget } from "./userWidget";
+import { useAuth } from "./authProvider";
 
-const NavLinks : Component<NavBarProps> = function (props) {
+const NavLinks: Component<NavBarProps> = function (props) {
 
     const handleScreenChange = (screenName: string) => {
         props.onSelectScreen(screenName);
@@ -50,10 +51,11 @@ const NavLinks : Component<NavBarProps> = function (props) {
 }
 
 interface NavBarProps {
-  onSelectScreen: (screenName: string) => void;
+    onSelectScreen: (screenName: string) => void;
 }
 
-const NavBar : Component<NavBarProps> = function (props) {
+const NavBar: Component<NavBarProps> = function (props) {
+    const { user, login, logout, loading } = useAuth();
 
     const handleScreenChange = (screenName: string) => {
         props.onSelectScreen(screenName);
@@ -66,16 +68,22 @@ const NavBar : Component<NavBarProps> = function (props) {
                     <div tabIndex={0} role="button" class="btn btn-ghost lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </div>
-                    <ul tabIndex={0} class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                        <NavLinks onSelectScreen={handleScreenChange} />
-                    </ul>
+                    {
+                        user() != null ?
+                            <ul tabIndex={0} class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                                <NavLinks onSelectScreen={handleScreenChange} />
+                            </ul> : <></>
+                    }
                 </div>
                 <a onClick={() => handleScreenChange("welcome")} class="btn btn-ghost text-xl">AlbionMC</a>
             </div>
             <div class="navbar-center hidden lg:flex">
-                <ul class="menu menu-horizontal px-1">
-                    <NavLinks onSelectScreen={handleScreenChange} />
-                </ul>
+                {
+                    user() != null ?
+                        <ul class="menu menu-horizontal px-1">
+                            <NavLinks onSelectScreen={handleScreenChange} />
+                        </ul> : <></>
+                }
             </div>
             <div class="navbar-end">
                 <UserWidget onSelectScreen={handleScreenChange} />
